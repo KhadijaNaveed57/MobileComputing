532deab2727ac6bb79664f6352e0330668aca587
KhadijaNaveed57 <mcaf19a0312pucit.edu.pk>
2021-06-09 00:55:22 +0500

    animation1

--- Lecture9_Project/app/src/main/java/com/khadijanaveed/lecture9_project/MainActivity.java
+++ Lecture9_Project/app/src/main/java/com/khadijanaveed/lecture9_project/MainActivity.java
@@ -3,6 +3,8 @@ package com.khadijanaveed.lecture9_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void animation(View view) {
+        ImageView imageView = findViewById(R.id.image);
+        imageView.animate().alpha(0).setDuration(500);
     }
 }
