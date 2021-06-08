1e7652fc528081543a885e9421f6d5ea25856071
KhadijaNaveed57 <mcaf19a0312pucit.edu.pk>
2021-06-09 01:01:57 +0500

    animations done

--- Lecture9_Project/app/src/main/java/com/khadijanaveed/lecture9_project/MainActivity.java
+++ Lecture9_Project/app/src/main/java/com/khadijanaveed/lecture9_project/MainActivity.java
@@ -16,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void animation(View view) {
         ImageView imageView = findViewById(R.id.image);
-        imageView.animate().alpha(0).setDuration(500);
+//        imageView.animate().alpha(0).setDuration(500);
+//        imageView.animate().translationXBy(-500).setDuration(500);
+//        imageView.animate().rotation(720).setDuration(2000);
+//        imageView.animate().rotation(720).alpha(0).setDuration(2000);
+        imageView.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
     }
 }
